e6f7b67f381c52a2ca485e71604307d9a7c3b2b1
grigoriank8 <karogrigorian8@gmail.com>
2024-08-07 19:20:38 +0400

    backend(AppConfig,JwtConstant,JwtTokenFilter &  app.yml)

---
+++ backend/src/main/java/com/grigoriank/backend/security/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.grigoriank.backend.security;
+
+public class JwtConstant {
+
+    public static final String JWT_HEADER = "Authorization";
+    public static final String SECRET_KEY = "aqrrewueowue;asjagsjaaspwopsxnxww;wehudiakskhuetpripewszs;skjksh";
+}
